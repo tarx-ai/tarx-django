948bc69f65e98da74e12ab516afbdb786521ad0e
BohdanSizomin <bohdansizomin@gmail.com>
2023-04-28 14:59:09 +0300

    extra docker config staticfiles folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd app
+
 echo Running migrations
 python manage.py migrate --noinput
+
+echo Collectin staticfiles
+python manage.py collectstatic --no-input --clear
+
 echo Running server with gunicorn
 gunicorn core.wsgi:application --workers 3 --bind 0.0.0.0:80
