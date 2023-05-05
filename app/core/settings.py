7971392d97fe1652b1bf251ec4940b1c51f7414a
BohdanSizomin <bohdansizomin@gmail.com>
2023-05-05 12:44:22 +0300

    settings db

--- app/core/settings.py
+++ app/core/settings.py
@@ -74,10 +74,10 @@ if os.getenv("ENV") == "production":
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": os.getenv("DATABASE_NAME"),
-            "USER": os.getenv("DATABASE_USERNAME"),
-            "PASSWORD": os.getenv("DATABASE_PASSWORD"),
-            "HOST": "localhost",
+            "NAME": os.getenv("DATABASE_NAME", "db"),
+            "USER": os.getenv("DATABASE_USERNAME", "postgres"),
+            "PASSWORD": os.getenv("DATABASE_PASSWORD", "password"),
+            "HOST": os.getenv("DATABASE_NAME", "db"),
             "PORT": os.getenv("DB_PORT", "5432"),
         },
     }
